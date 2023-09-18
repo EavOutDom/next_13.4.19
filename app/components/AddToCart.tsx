2704aaa84ca0c6d4775033c1d303b79ee40ed266
EavOutDom <eavoutdom400@gmail.com>
2023-09-18 11:12:08 +0700

    global style

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button className="btn btn-primary">AddToCart</button>
+    </div>
+  );
+};
+
+export default AddToCart;
---
+++ app/users/page.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+interface Users {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: { revalidate: 1 },
+  });
+  const users: Users[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default UsersPage;
