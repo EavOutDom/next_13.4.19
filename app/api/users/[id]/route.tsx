a8679d1abc2d7b52f625f84fcd3206f4005783ef
EavOutDom <eavoutdom400@gmail.com>
2023-09-21 17:26:49 +0700

    updating data

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -17,17 +17,24 @@ export async function GET(
 
 export async function PUT(
   request: NextRequest,
-  { params: { id } }: { params: { id: number } }
+  { params: { id } }: { params: { id: string } }
 ) {
   const body = await request.json();
   const validation = schema.safeParse(body);
   if (!validation.success) {
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
-  if (id > 1) {
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(id) },
+  }); 
+  if (!user) {
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   }
-  return NextResponse.json({ id, name: body.name });
+  const updatedUser = await prisma.user.update({
+    where: { id: user.id },
+    data: body,
+  });
+  return NextResponse.json(updatedUser);
 }
 
 export async function DELETE(
