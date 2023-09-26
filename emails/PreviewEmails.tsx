2a458bf909b5a82bc8bfcffdef566e1cd0f2f4bd
EavOutDom <eavoutdom400@gmail.com>
2023-09-26 08:42:16 +0700

    react-email in codesandbox

---
+++ emails/PreviewEmails.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Html, Button } from "@react-email/components";
+
+export default function Email() {
+  return (
+    <Html>
+      <Button
+        pX={20}
+        pY={12}
+        href="https://example.com"
+        style={{ background: "#000", color: "#fff" }}
+      >
+        Click me
+      </Button>
+    </Html>
+  );
+}
